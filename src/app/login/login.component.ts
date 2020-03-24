6b5fcb1698443f9ece2d3c9060eec4b109572677
Swati <swati.jadhav@aventior.com>
2020-03-24 17:39:52 +0530

    working on login functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+// import { url } from 'inspector';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+  ipAddress = "http://127.0.0.1:8000"
+  // ipAddress = "3.6.69.205:8000"
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({ 
+      'Access-Control-Allow-Origin':'*',
+      'Content-Type': 'application/json'
+    })
+  };
+  
+  loginApi = this.ipAddress + "/api/login"
+  public userName: string
+  public passWord: string
+  public loginBody: any
 
+  constructor(private httpclient: HttpClient, private http: HttpClient) { }
   ngOnInit() {
+  }
+
+  onLogin() {
+    console.log("username & Password is =>>> " + this.userName, this.passWord)
+    // this.loginBody = { "body": { "username": this.userName, "password": this.passWord } }
+    // this.httpclient.post(this.loginApi, this.loginBody).subscribe(
+    //   data => {
+    //     console.log("data====>" + data)
+    //   }
+    // )
+    console.log("Clicked On Login Button")
   }
 
 }
